25dac80c6a40430d949eeae5e6b0acbf47e55d18
AimaZubar <Aimakh@users.noreply.github.com>
2021-03-19 22:45:04 +0500

    Template of Hotel About completed.
    Design 2 is Hotel About template complete using padding containers and icons buttons.

--- sessional1_01_009/lib/design2.dart
+++ sessional1_01_009/lib/design2.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -131,8 +132,7 @@ class MyApp extends StatelessWidget {
                                   right: 0.0,
                                   bottom: 0.0),
                               child: Text(
-                                'A',
-                                maxLines: 2,
+                                'Amanities',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontWeight: FontWeight.bold,
@@ -146,9 +146,92 @@ class MyApp extends StatelessWidget {
                           children: <Widget>[
                             Padding(
                               padding: const EdgeInsets.only(
-                                  left: 9.0, top: 8.0, right: 0.0, bottom: 0.0),
+                                  left: 15.0,
+                                  top: 35.0,
+                                  right: 0.0,
+                                  bottom: 0.0),
                               child: Icon(
-                                Icons.brightness_5,
+                                Icons.wifi,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 115.0,
+                                  top: 35.0,
+                                  right: 0.0,
+                                  bottom: 0.0),
+                              child: Icon(
+                                Icons.flight,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 65.0,
+                                  top: 35.0,
+                                  right: 0.0,
+                                  bottom: 0.0),
+                              child: Icon(
+                                Icons.brightness_2,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 170.0,
+                                  top: 35.0,
+                                  right: 0.0,
+                                  bottom: 0.0),
+                              child: Icon(
+                                Icons.headset,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 235.0,
+                                  top: 35.0,
+                                  right: 0.0,
+                                  bottom: 0.0),
+                              child: Icon(
+                                Icons.settings_bluetooth,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 300.0,
+                                  top: 35.0,
+                                  right: 0.0,
+                                  bottom: 0.0),
+                              child: Icon(
+                                Icons.settings_sharp,
                                 color: Colors.white,
                               ),
                             ),
@@ -159,6 +242,158 @@ class MyApp extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 8.0, top: 8.0, right: 0.0, bottom: 0.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 345.0,
+                    height: 120.0,
+                    color: Colors.grey,
+                    child: Stack(
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 9.0,
+                                  top: 30.0,
+                                  right: 0.0,
+                                  bottom: 0.0),
+                              child: Icon(
+                                Icons.radio_button_on,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 15.0,
+                                  top: 8.0,
+                                  right: 0.0,
+                                  bottom: 0.0),
+                              child: Text(
+                                'Where is that',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 40.0,
+                                  top: 28.0,
+                                  right: 0.0,
+                                  bottom: 0.0),
+                              child: Text(
+                                'Address\n152 Taylor street, NYC',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 10.0,
+                                  top: 10.0,
+                                  right: 0.0,
+                                  bottom: 0.0),
+                              child: RaisedButton(
+                                onPressed: () => {},
+                                child: Text('Check it >'),
+                                color: Color(0xffFF1744),
+                                textColor: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Stack(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 8.0, top: 8.0, right: 0.0, bottom: 0.0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 345.0,
+                        height: 150.0,
+                        color: Colors.grey,
+                        child: Stack(
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20.0,
+                                      top: 10.0,
+                                      right: 0.0,
+                                      bottom: 0.0),
+                                  child: Text(
+                                    'Before you go',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20.0,
+                                      top: 10.0,
+                                      right: 0.0,
+                                      bottom: 0.0),
+                                  child: Text(
+                                    '- Payment at checkout\n'
+                                    '- Wifi Network is off by 12\n'
+                                    '- No more than two bags\n'
+                                    '- No Refund',
+                                    style: TextStyle(
+                                      fontSize: 12.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 25.0, top: 135.0, right: 25.0, bottom: 0.0),
+                  child: SizedBox(
+                      width: 320.0,
+                      child: RaisedButton(
+                        onPressed: () => {},
+                        child: Text('Back to Home'),
+                        color: Color(0xffFF1744),
+                        textColor: Colors.white,
+                      )),
+                ),
+              ],
             ),
           ],
         ),
