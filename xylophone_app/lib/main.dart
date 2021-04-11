ef16e448c1c77dc480612707740128c640a933bd
AimaZubar <Aimakh@users.noreply.github.com>
2021-04-11 21:21:19 +0500

    Custom Selection

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -9,105 +9,103 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: DropDown(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class DropDown extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  DropDownWidget createState() => DropDownWidget();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class DropDownWidget extends State<DropDown> {
+  String dropdownValue = 'blue';
 
-  void _incrementCounter() {
+  // To show Selected Item in Text.
+  String holder = '';
+
+  List<String> colorName = [
+    'blue',
+    'green',
+    'purple',
+    'pink',
+    'white',
+    'teal',
+    'red'
+  ];
+
+  void getDropDownItem() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      holder = dropdownValue;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        backgroundColor: Colors.blue,
+        title: Text(
+          'XyloPhone App',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: <Widget>[
+          DropdownButton(
+            value: dropdownValue,
+            icon: Icon(Icons.arrow_drop_down),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.red, fontSize: 18),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String data) {
+              setState(() {
+                dropdownValue = data;
+              });
+            },
+            items: colorName.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+          Padding(
+              padding: EdgeInsets.only(top: 30, bottom: 30),
+              child:
+                  //Printing Item on Text Widget
+                  Text('Selected Item = ' + '$holder',
+                      style: TextStyle(fontSize: 22, color: Colors.black))),
+          RaisedButton(
+            child: Text('Next'),
+            onPressed: getDropDownItem,
+            color: Colors.blue,
+            textColor: Colors.white,
+            padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class XyoloPhone extends StatefulWidget {
+  @override
+  _XyoloPhoneState createState() => _XyoloPhoneState();
+}
+
+class _XyoloPhoneState extends State<XyoloPhone> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
