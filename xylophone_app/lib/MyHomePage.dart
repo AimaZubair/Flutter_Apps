2ddb7f0c7863e432cd95a0df117da4e9001a649b
AimaZubar <Aimakh@users.noreply.github.com>
2021-04-12 00:53:50 +0500

    Some changes

---
+++ xylophone_app/lib/MyHomePage.dart
@@ -0,0 +1,44 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+          color: color,
+          onPressed: () {
+            playSound(soundNumber);
+          }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Color($holder), soundNumber: 1),
+              buildKey(color: Colors.yellow[200], soundNumber: 2),
+              buildKey(color: Colors.tealAccent, soundNumber: 3),
+              buildKey(color: Colors.purple[200], soundNumber: 4),
+              buildKey(color: Colors.grey, soundNumber: 5),
+              buildKey(color: Colors.blueAccent, soundNumber: 6),
+              buildKey(color: Colors.orange[200], soundNumber: 7)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,10 +22,13 @@ class DropDown extends StatefulWidget {
 }
 
 class DropDownWidget extends State<DropDown> {
+  String holder = '';
+  String holder_2 = '';
+
   String dropdownValue = 'blue';
+  String dropdownValue_2 = '1';
 
   // To show Selected Item in Text.
-  String holder = '';
 
   List<String> colorName = [
     'blue',
@@ -35,10 +39,12 @@ class DropDownWidget extends State<DropDown> {
     'teal',
     'red'
   ];
+  List<String> soundNo = ['1', '2', '3', '4', '5', '6', '7'];
 
   void getDropDownItem() {
     setState(() {
       holder = dropdownValue;
+      holder_2 = dropdownValue_2;
     });
   }
 
@@ -79,11 +85,39 @@ class DropDownWidget extends State<DropDown> {
               );
             }).toList(),
           ),
+          DropdownButton(
+            value: dropdownValue_2,
+            icon: Icon(Icons.arrow_drop_down),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.red, fontSize: 18),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String data) {
+              setState(() {
+                dropdownValue_2 = data;
+              });
+            },
+            items: soundNo.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
           Padding(
               padding: EdgeInsets.only(top: 30, bottom: 30),
               child:
                   //Printing Item on Text Widget
                   Text('Selected Item = ' + '$holder',
+                      style: TextStyle(fontSize: 22, color: Colors.black))),
+          Padding(
+              padding: EdgeInsets.only(top: 30, bottom: 30),
+              child:
+                  //Printing Item on Text Widget
+                  Text('Selected Item = ' + '$holder_2',
                       style: TextStyle(fontSize: 22, color: Colors.black))),
           RaisedButton(
             child: Text('Next'),
@@ -95,17 +129,5 @@ class DropDownWidget extends State<DropDown> {
         ],
       ),
     );
-  }
-}
-
-class XyoloPhone extends StatefulWidget {
-  @override
-  _XyoloPhoneState createState() => _XyoloPhoneState();
-}
-
-class _XyoloPhoneState extends State<XyoloPhone> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
