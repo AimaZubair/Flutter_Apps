e1a8cec6060e59e3b28f0eed7b752ac9cb9f438a
AimaZubar <Aimakh@users.noreply.github.com>
2021-04-12 18:30:33 +0500

    Quiz App

--- quiz_app/lib/QuizApp.dart
+++ quiz_app/lib/QuizApp.dart
@@ -19,25 +19,32 @@ class _QuizAppState extends State<QuizApp> {
         'No piece of square dry paper can be folded in half more than 7 times.',
         false),
     Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        true),
-    Question(
-        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        false),
-    Question(
         'The total surface area of two human lungs is approximately 70 square metres.',
         true),
     Question('Google was originally called \"Backrub\".', true),
     Question(
         'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
         true),
-    Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        true),
   ];
 
   int questionIndex = 0;
   String answer = '';
+  int correct = 0;
+  bool _buttondisable = false;
+
+  void _showalert() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text("Thank you"),
+              content: Text("Total Correct Answer $correct"),
+            ));
+  }
+
+  void disable() {
+    _buttondisable = true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -100,10 +107,15 @@ class _QuizAppState extends State<QuizApp> {
                   ),
                 ),
                 onTap: () {
+                  if (questionIndex == 10) {
+                    _showalert();
+                    disable();
+                  }
                   if (questions[questionIndex].answer == true) {
                     setState(() {
                       questionIndex++;
                       answer = 'Correct';
+                      correct++;
                     });
                   } else {
                     setState(() {
@@ -129,10 +141,15 @@ class _QuizAppState extends State<QuizApp> {
                   ),
                 ),
                 onTap: () {
+                  if (questionIndex == 10) {
+                    _showalert();
+                    disable();
+                  }
                   if (questions[questionIndex].answer == false) {
                     setState(() {
                       questionIndex++;
                       answer = 'Correct';
+                      correct++;
                     });
                   } else {
                     setState(() {
--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -9,6 +9,7 @@ class Quiz_App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.green,
