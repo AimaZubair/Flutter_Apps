f7944177010a8b27709bf9851b05571f7183c59b
AimaZubar <Aimakh@users.noreply.github.com>
2021-04-19 16:35:51 +0500

    Splash Screen
    Screen with roll no and pic

---
+++ quiz_app_assignment/lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'MyHomePage.dart';
+
+import 'package:splashscreen/splashscreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splash(),
+    );
+  }
+}
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 3,
+      navigateAfterSeconds: MyHomePage(),
+      title: Text(
+        'FA17-BCS-009',
+        style: TextStyle(
+          fontSize: 10.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      image: Image.asset('images/AA.png'),
+      loadingText: Text("Loading"),
+      photoSize: 100.0,
+      loaderColor: Colors.blue,
+    );
+  }
+}
